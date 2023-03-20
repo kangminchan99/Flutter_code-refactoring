63773e9d58a295b87ed9d62e39b6bf8f5ba4389a
kangminchan99 <114412280+kangminchan99@users.noreply.github.com>
2023-03-20 21:57:57 +0900

    code refactoring

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_refactoring/utils/login_button.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
@@ -15,55 +16,67 @@ class LoginPage extends StatelessWidget {
         backgroundColor: Colors.white,
       ),
       body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ElevatedButton(
-                  style:
-                      ElevatedButton.styleFrom(backgroundColor: Colors.white),
-                  onPressed: () {},
-                  child: Row(
-                    children: [
-                      Image.asset('images/glogo.png'),
-                      const SizedBox(width: 30),
-                      const Text(
-                        'Login with Google',
-                        style: TextStyle(color: Colors.black),
-                      ),
-                    ],
-                  )),
-              ElevatedButton(
-                  style: ElevatedButton.styleFrom(backgroundColor: Colors.blue),
-                  onPressed: () {},
-                  child: Row(
-                    children: [
-                      Image.asset('images/flogo.png'),
-                      const SizedBox(width: 30),
-                      const Text(
-                        'Login with Facebook',
-                        style: TextStyle(color: Colors.white),
-                      ),
-                    ],
-                  )),
-              ElevatedButton(
-                  style:
-                      ElevatedButton.styleFrom(backgroundColor: Colors.green),
-                  onPressed: () {},
-                  child: Row(
-                    children: const [
-                      Icon(Icons.email),
-                      SizedBox(width: 35),
-                      Text(
-                        'Login with Email',
-                        style: TextStyle(color: Colors.white),
-                      ),
-                    ],
-                  ))
-            ],
-          ),
-        ),
+        child: buildButton(),
+      ),
+    );
+  }
+
+  //  _ : 접근 제어자 => private (같은 파일 내에서만 접근 가능)
+  // ex) _buildButton
+  Widget buildButton() {
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          ElevatedButton(
+              style: ElevatedButton.styleFrom(backgroundColor: Colors.white),
+              onPressed: () {},
+              child: Row(
+                children: [
+                  Image.asset('images/glogo.png'),
+                  const SizedBox(width: 30),
+                  const Text(
+                    'Login with Google',
+                    style: TextStyle(color: Colors.black),
+                  ),
+                ],
+              )),
+          // ex) code refactoring
+          LoginButton(
+              image: Image.asset('images/glogo.png'),
+              text: const Text(
+                'Login with Google',
+                style: TextStyle(color: Colors.black),
+              ),
+              onPressed: () {}),
+          ElevatedButton(
+              style: ElevatedButton.styleFrom(backgroundColor: Colors.blue),
+              onPressed: () {},
+              child: Row(
+                children: [
+                  Image.asset('images/flogo.png'),
+                  const SizedBox(width: 30),
+                  const Text(
+                    'Login with Facebook',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ],
+              )),
+          ElevatedButton(
+              style: ElevatedButton.styleFrom(backgroundColor: Colors.green),
+              onPressed: () {},
+              child: Row(
+                children: const [
+                  Icon(Icons.email),
+                  SizedBox(width: 35),
+                  Text(
+                    'Login with Email',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ],
+              ))
+        ],
       ),
     );
   }
--- lib/utils/login_button.dart
+++ lib/utils/login_button.dart
@@ -1,21 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_refactoring/login_page.dart';
 
 class LoginButton extends StatelessWidget {
-  const LoginButton({super.key});
+  // this 앞에 required를 붙이는 것은 매개변수가 항상 지정되어야 함을 나타낸다.
+  const LoginButton(
+      {required this.image,
+      required this.text,
+      required this.onPressed,
+      super.key});
+
+  // final 변수들은 한번 초기화가 되면 그 이후에는 절대로 변하지 않으나,
+  // 로그인 페이지에서 새로운 값들이 생성자를 통해 전달되면 StatelessWidget 특성상
+  // build method내의 모든 위젯들이 리빌딩 되는 과정에서 이 변수들 또한 리빌딩 되어
+  // 새로운 값이 적용된다.
+  final Widget image;
+  final Widget text;
+  final VoidCallback onPressed;
 
   @override
   Widget build(BuildContext context) {
+    LoginPage login = const LoginPage();
+    login.buildButton();
+
     return ElevatedButton(
         style: ElevatedButton.styleFrom(backgroundColor: Colors.white),
-        onPressed: () {},
+        onPressed: onPressed,
         child: Row(
           children: [
-            Image.asset('images/glogo.png'),
+            image,
             const SizedBox(width: 30),
-            const Text(
-              'Login with Google',
-              style: TextStyle(color: Colors.black),
-            ),
+            text,
           ],
         ));
   }
